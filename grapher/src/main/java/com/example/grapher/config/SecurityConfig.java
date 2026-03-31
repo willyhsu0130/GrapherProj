13d7cc6aad84fa804663bb8add4c46ede7aaade1
Willy Hsu <N01678692@humber.ca>
2026-03-31 16:07:01 -0400

    added katalon test-client to cors

--- grapher/src/main/java/com/example/grapher/config/SecurityConfig.java
+++ grapher/src/main/java/com/example/grapher/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(frontendUrl));
+        config.setAllowedOrigins(List.of(frontendUrl, "katalon://test-client") );
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
